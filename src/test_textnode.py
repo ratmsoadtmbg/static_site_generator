2c810cbf5973c45b271af2cb087dfe833c688b72
ratmsoadtmbg <pariahsdog@gmail.com>
2025-06-03 00:19:50 -0600

    Added test functionality

---
+++ src/test_textnode.py
@@ -0,0 +1,34 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.ITALIC, "https://www.youtube.com")
+        node_test = "TextNode(This is a text node, italic, https://www.youtube.com)"
+        self.assertEqual(str(node), node_test)
+
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is b text node", TextType.IMAGE, "https://urmom.lol")
+        self.assertNotEqual(node, node2)
+
+    def test_none_url(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD, None)
+        self.assertEqual(node, node2)
+
+    def test_wrong_type(self):
+        try:
+            node = TextNode("text node", TextType.BLOOD)
+        except Exception as e:
+            print(f"Error encountered: {e}")
+
+
+if __name__ == "__main__":
+    unittest.main()
